f4c501a8719c71bd2668760cfd701ec973266a60
ray <yjr.ray@qq.com>
2026-07-24 03:27:44 +0000

    add always broken

--- src/main/java/com/stocks/service/StockServiceImpl.java
+++ src/main/java/com/stocks/service/StockServiceImpl.java
@@ -32,10 +32,8 @@ public class StockServiceImpl implements StockService {
 
     @Override
     public Stock addStock(Stock stock) {
-        stockRepository.findBySymbol(stock.symbol()).ifPresent(s -> {
-            throw new IllegalArgumentException("Duplicate symbol: " + stock.symbol());
-        });
-        return stockRepository.save(stock);
+        // Temporarily break the duplicate check
+        throw new IllegalArgumentException("Always broken: " + stock.symbol());
     }
 
     @Override
